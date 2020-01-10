904fe7602c301a8d811852b1dd6d57c1402248e5
HTC Vive workstation <MrNobody@NoWhere.com>
2020-01-10 13:44:15 +0200

    Climbing

--- JungleAndChill/Assets/Scripts/Climber.cs
+++ JungleAndChill/Assets/Scripts/Climber.cs
@@ -2,12 +2,11 @@
 using Valve.VR;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Climber : MonoBehaviour
-{
+public class Climber : MonoBehaviour {
     public ClimberHand RightHand;
     public ClimberHand LeftHand;
     public SteamVR_Action_Boolean ToggleGripButton;
-    public SteamVR_Action_Pose position;
+    //public SteamVR_Action_Pose position;
     public ConfigurableJoint ClimberHandle;
 
     private bool Climbing;
@@ -16,24 +15,24 @@ public class Climber : MonoBehaviour
     void Update() {
         updateHand(RightHand);
         updateHand(LeftHand);
-        if(Climbing) {
+        if (Climbing) {
             ClimberHandle.targetPosition = -ActiveHand.transform.localPosition;//update collider for hand movment
         }
     }
 
     void updateHand(ClimberHand Hand) {
-        if(Climbing && Hand == ActiveHand)//if is the hand used for climbing check if we are letting go.
+        if (Climbing && Hand == ActiveHand)//if is the hand used for climbing check if we are letting go.
         {
-            if(ToggleGripButton.GetStateUp(Hand.Hand)) {
+            if (ToggleGripButton.GetStateUp(Hand.Hand)) {
                 ClimberHandle.connectedBody = null;
                 Climbing = false;
 
                 GetComponent<Rigidbody>().useGravity = true;
             }
         } else {
-            if(ToggleGripButton.GetStateDown(Hand.Hand) || Hand.grabbing) {
+            if (ToggleGripButton.GetStateDown(Hand.Hand) || Hand.grabbing) {
                 Hand.grabbing = true;
-                if(Hand.TouchedCount > 0) {
+                if (Hand.TouchedCount > 0) {
                     ActiveHand = Hand;
                     Climbing = true;
                     ClimberHandle.transform.position = Hand.transform.position;
--- JungleAndChill/Assets/Scripts/ClimberHand.cs
+++ JungleAndChill/Assets/Scripts/ClimberHand.cs
@@ -1,18 +1,17 @@
 ﻿using UnityEngine;
 using Valve.VR;
-public class ClimberHand : MonoBehaviour
-{
+public class ClimberHand : MonoBehaviour {
     public SteamVR_Input_Sources Hand;
     public int TouchedCount;
     public bool grabbing;
 
     void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Climbable")) {
+        if (other.CompareTag("Climbable")) {
             TouchedCount++;
         }
     }
     void OnTriggerExit(Collider other) {
-        if(other.CompareTag("Climbable")) {
+        if (other.CompareTag("Climbable")) {
             TouchedCount--;
         }
     }

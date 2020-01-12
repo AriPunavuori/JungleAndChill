336827fcb376ebc15be6761a2060d41d5ddbd187
Samuel <32210043+Satsaa@users.noreply.github.com>
2020-01-13 00:12:09 +0200

    Pohjaa

---
+++ JungleAndChill/Assets/Scripts/Boomerang.cs
@@ -0,0 +1,94 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+//-------------------------------------------------------------------------
+[RequireComponent(typeof(Interactable))]
+public class Boomerang : MonoBehaviour {
+  private Vector3 oldPosition;
+  private Quaternion oldRotation;
+
+  private float attachTime;
+
+  private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+  private Interactable interactable;
+
+  //-------------------------------------------------
+  // Called when a Hand starts hovering over this object
+  //-------------------------------------------------
+  private void OnHandHoverBegin(Hand hand) {
+
+  }
+
+
+  //-------------------------------------------------
+  // Called when a Hand stops hovering over this object
+  //-------------------------------------------------
+  private void OnHandHoverEnd(Hand hand) {
+
+  }
+
+
+  //-------------------------------------------------
+  // Called every Update() while a Hand is hovering over this object
+  //-------------------------------------------------
+  private void HandHoverUpdate(Hand hand) {
+    GrabTypes startingGrabType = hand.GetGrabStarting();
+    bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+    if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None) {
+      // Save our position/rotation so that we can restore it when we detach
+      oldPosition = transform.position;
+      oldRotation = transform.rotation;
+
+      // Call this to continue receiving HandHoverUpdate messages,
+      // and prevent the hand from hovering over anything else
+      hand.HoverLock(interactable);
+
+      // Attach this object to the hand
+      hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+    } else if (isGrabEnding) {
+      // Detach this object from the hand
+      hand.DetachObject(gameObject);
+
+      // Call this to undo HoverLock
+      hand.HoverUnlock(interactable);
+
+      // Restore position/rotation
+      transform.position = oldPosition;
+      transform.rotation = oldRotation;
+    }
+  }
+
+
+  //-------------------------------------------------
+  // Called when this GameObject becomes attached to the hand
+  //-------------------------------------------------
+  private void OnAttachedToHand(Hand hand) {
+  }
+
+
+
+  //-------------------------------------------------
+  // Called when this GameObject is detached from the hand
+  //-------------------------------------------------
+  private void OnDetachedFromHand(Hand hand) {
+  }
+
+
+  //-------------------------------------------------
+  // Called every Update() while this GameObject is attached to the hand
+  //-------------------------------------------------
+  private void HandAttachedUpdate(Hand hand) {
+  }
+
+  private bool lastHovering = false;
+  private void Update() {
+    if (interactable.isHovering != lastHovering) //save on the .tostrings a bit
+    {
+    }
+  }
+}
---
+++ JungleAndChill/Assets/Scripts/HoldHandles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class HoldHandles : MonoBehaviour {
+  [Tooltip("How much angular difference affects the calculation of closest position. The calculated distance to a position is multiplied by: 1 - (180 / angleDifference  * " + nameof(angleWeight) + ")")]
+  public float angleWeight = 1;
+  public Transform[] handles;
+
+  private void OnAttachedToHand(Hand hand) {
+    var handPos = hand.transform.position;
+
+    // Find closest pos
+    Transform selected = null;
+    float minDist = float.PositiveInfinity;
+    foreach (var pos in handles) {
+      var dist = Vector3.Distance(pos.position, handPos);
+      // Factor in angle difference
+      if (angleWeight != 0)
+        dist = dist * (1 - (180 / Vector3.Angle(pos.rotation.eulerAngles, hand.transform.rotation.eulerAngles) * angleWeight));
+      if (dist < minDist) {
+        selected = pos;
+        minDist = dist;
+      }
+    }
+    // Move to selected position
+    if (selected != null) {
+      // Set position so that selected handle is at hand position
+      transform.position = hand.transform.position + (transform.position - selected.position);
+      // Set angle so that selected angle matches hand angle
+      transform.rotation = selected.rotation;
+    }
+  }
+}

b52db77dc0094bb9806fd583417269e188cba8ea
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-01-14 13:25:19 +0200

    Level check

--- JungleAndChill/Assets/Scripts/Climber.cs
+++ JungleAndChill/Assets/Scripts/Climber.cs
@@ -4,15 +4,19 @@ using Valve.VR;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Climber : MonoBehaviour {
+    LayerMask mask;
+    Rigidbody rb;
+    float rayLength = 0.1f;
     public ClimberHand RightHand;
     public ClimberHand LeftHand;
     public SteamVR_Action_Boolean ToggleGripButton;
-    //public SteamVR_Action_Pose position;
     public ConfigurableJoint ClimberHandle;
-
-    //    private bool Climbing;
     public List<ClimberHand> grabbedHands = new List<ClimberHand>(); // first in list is active
 
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+        mask = LayerMask.NameToLayer("Floor");
+    }
     bool IsClimbing() {
         return grabbedHands.Count > 0;
     }
@@ -23,8 +27,12 @@ public class Climber : MonoBehaviour {
     void Update() {
         UpdateHand(RightHand);
         UpdateHand(LeftHand);
-        if (IsClimbing()) {
+        if(IsClimbing()) {
             ClimberHandle.targetPosition = -ActiveHand().transform.localPosition;//update collider for hand movment
+        } else if(Physics.Raycast(transform.position, Vector3.down, rayLength, mask)) {
+            rb.useGravity = false;
+        } else {
+            rb.useGravity = true;
         }
     }
 
@@ -33,19 +41,16 @@ public class Climber : MonoBehaviour {
             if (ToggleGripButton.GetStateUp(hand.hand)) {
                 grabbedHands.Remove(hand);
                 if (!IsClimbing()) {
-                    GetComponent<Rigidbody>().useGravity = true;
+                    rb.useGravity = true;
                     ClimberHandle.connectedBody = null;
-                } else {
-                    // position?
                 }
-
             }
         } else {
             if (hand.touchedCount > 0 && ToggleGripButton.GetStateDown(hand.hand)) {
                 grabbedHands.Insert(0, hand);
                 print("adding hand " + hand.name + " to list idx 0");
                 ClimberHandle.transform.position = hand.transform.position;
-                GetComponent<Rigidbody>().useGravity = false;
+                rb.useGravity = false;
                 ClimberHandle.connectedBody = GetComponent<Rigidbody>();
             }
         }

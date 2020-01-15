fa8fadb2cbd1b6d9d942c1e15a4a770788d21ae1
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-01-15 14:13:08 +0200

    Daytime switching

---
+++ JungleAndChill/Assets/Scripts/DayTimeSwitching/BallReturn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReturn : MonoBehaviour{
+    Vector3 returnPoint;
+    Rigidbody rb;
+    float returnTime;
+    public float returnTimer;
+    bool ballThrown;
+    private void Start() {
+        returnTime = GameObject.Find("DaytimeSwitcher").GetComponent<DaytimeSwitcher>().switchTime;
+        returnPoint = transform.position;
+        returnTimer = returnTime;
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update() {
+        if(ballThrown)
+            returnTimer -= Time.deltaTime;
+        if (returnTimer < 0) {
+            rb.MovePosition(returnPoint);
+            ballThrown = false;
+        }
+    }
+    public void BallThrown() {
+        returnTimer = returnTime;
+        ballThrown = true;
+    }
+}
---
+++ JungleAndChill/Assets/Scripts/DayTimeSwitching/DaytimeSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaytimeSwitcher : MonoBehaviour {
+    public enum dayNite { Day, Night, DayToNight, NightToDay };
+    dayNite dn;
+    public float Daylight;
+    public float switchTime = 5;
+
+    private void Update() {
+        if(dn == dayNite.DayToNight) {
+            Daylight -= (Time.deltaTime / switchTime);
+        }
+        if(dn == dayNite.NightToDay) {
+            Daylight += (Time.deltaTime / switchTime);
+        }
+        if(Daylight <= 0) {
+            dn = dayNite.Night;
+            Daylight = 0;
+        }
+        if(Daylight >= 1) {
+            dn = dayNite.Day;
+            Daylight = 1;
+        }
+    }
+
+    void SwitchDaytime() {
+        if(dn == dayNite.Day)
+            dn = dayNite.DayToNight;
+        else
+            dn = dayNite.NightToDay;
+    }
+    private void OnTriggerExit(Collider other) {
+        if(other.CompareTag("DaytimeSwitcher")) {
+            SwitchDaytime();
+        }
+    }
+
+}

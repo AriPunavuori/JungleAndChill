d2766d8df60841d2119f805f3861a38ab3dd5a6d
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-01-17 12:10:39 +0200

    VineDropper

--- JungleAndChill/Assets/Scripts/VineDropper/VineDropper.cs
+++ JungleAndChill/Assets/Scripts/VineDropper/VineDropper.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class VineDropper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    GameObject vine;
+    Vector3 target;
+    float dropSpeed;
+    bool first;
+    bool second;
+    bool third;
+    bool startVineDropping;
+
+    void Start() {
+        vine = GameObject.Find("Vine");
+        target = vine.transform.position + Vector3.down * 5;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "FirstVineObject")
+            first = true;
+        if(other.tag == "SecondVineObject")
+            second = true;
+        if(other.tag == "ThirdVineObject")
+            third = true;
+        if(first && second && third)
+            StartCoroutine("MoveToPosition");
+    }
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove) {
+        var currentPos = transform.position;
+        var t = 0f;
+        while(t < 1) {
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
     }
 }

0de45058ba71dff7d8324f8b227c9fbb105f630c
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-01-17 13:15:47 +0200

    VineDropper toimii

--- JungleAndChill/Assets/Scripts/VineDropper/VineDropper.cs
+++ JungleAndChill/Assets/Scripts/VineDropper/VineDropper.cs
@@ -6,10 +6,10 @@ public class VineDropper : MonoBehaviour
 {
     GameObject vine;
     Vector3 target;
-    float dropSpeed;
-    bool first;
-    bool second;
-    bool third;
+    float dropTime = 5f;
+    public bool first;
+    public bool second;
+    public bool third;
     bool startVineDropping;
 
     void Start() {
@@ -18,21 +18,24 @@ public class VineDropper : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
+        print("osui");
         if(other.tag == "FirstVineObject")
             first = true;
         if(other.tag == "SecondVineObject")
             second = true;
         if(other.tag == "ThirdVineObject")
             third = true;
-        if(first && second && third)
+        if(first && second && third) {
             StartCoroutine("MoveToPosition");
+            print("Start Dropping");
+        }
     }
-    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove) {
-        var currentPos = transform.position;
+    public IEnumerator MoveToPosition() {
+        var currentPos = vine.transform.position;
         var t = 0f;
         while(t < 1) {
-            t += Time.deltaTime / timeToMove;
-            transform.position = Vector3.Lerp(currentPos, position, t);
+            t += Time.deltaTime / dropTime;
+            vine.transform.position = Vector3.Lerp(currentPos, target, t);
             yield return null;
         }
     }

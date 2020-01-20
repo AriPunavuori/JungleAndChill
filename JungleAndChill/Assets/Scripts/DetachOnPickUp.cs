d76849ae0e2b6a388166fcb4e809d250c575c231
HTC Vive workstation <MrNobody@NoWhere.com>
2020-01-20 14:05:41 +0200

    SceneTuning

---
+++ JungleAndChill/Assets/Scripts/DetachOnPickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+//[RequireComponent(typeof(FixedJoint))]
+[RequireComponent(typeof(Interactable))]
+public class DetachOnPickUp : MonoBehaviour
+{
+    Interactable inter;
+    FixedJoint joint;
+
+    void Start()
+    {
+        inter= GetComponent<Interactable>();
+        inter.onAttachedToHand += OnAttach;
+        joint = GetComponent<FixedJoint>();
+    }
+
+    void OnAttach(Hand hand)
+    {
+        Destroy(joint);
+        //joint.connectedBody = null;
+    }
+}
